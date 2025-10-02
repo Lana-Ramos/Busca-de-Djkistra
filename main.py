3b7440a53c81b73fac9b794f291512b29797195d
Pedro Gabriel Ferraz Santos Silva <144126732+PGFerraz@users.noreply.github.com>
2025-10-01 21:44:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,22 @@ def abrir_grafo(arquivo):
     except json.JSONDecodeError:
         print("Erro: Arquivo não está em formato JSON válido.")
         return None
+
+def dijkstra(grafo, origem, destino):
+    fila = [(0, origem)]
+    distancias = {no: float("inf") for no in grafo}
+    distancias[origem] = 0
+    predecessores = {no: None for no in grafo}
+
+    while fila:
+        custo_atual, no_atual = heapq.heappop(fila)
+
+        if no_atual == destino:
+            break
+
+        for vizinho, peso in grafo[no_atual].items():
+            novo_custo = custo_atual + peso
+            if novo_custo < distancias[vizinho]:
+                distancias[vizinho] = novo_custo
+                predecessores[vizinho] = no_atual
+                heapq.heappush(fila, (novo_custo, vizinho))
